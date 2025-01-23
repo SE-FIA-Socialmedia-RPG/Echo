3ac54eeae049b63a22960e64c4048e8511ada28c
Andre Seiler <andre.01156@web.de>
2025-01-23 09:52:33 +0100

    Die NavigationBar.vue Komponente wurde in app,vue eingepflegt und dem template hinzugefügt

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import NavigationBar from '@/components/NavigationBar.vue'
+
+const appName = ref<string>('Social Media RPG')
+</script>
+
+
 <template>
   <UContainer>
+    <div id="app">
+    <NavigationBar />
     
+    </div>
   </UContainer>
 </template>

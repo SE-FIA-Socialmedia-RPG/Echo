b706a8fb0893f25bd0f6149a7df70ac76453100e
Andre Seiler <andre.01156@web.de>
2025-01-23 09:45:53 +0100

    Components Ordner erstellt und NavigationBar.vue mit UpperCamelCase umbenannt und dem Components Ordner hinzugefügt

---
+++ components/NavigationBar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const state = reactive({
+  title: "Navigation Bar",
+});
+
+const updateTitle = (newTitle: string) => {
+  state.title = newTitle;
+};
+
+const links = [
+  [
+    {
+      label: "Installation",
+      icon: "i-heroicons-home",
+      to: "/getting-started/installation",
+    },
+    {
+      label: "Horizontal Navigation",
+      icon: "i-heroicons-chart-bar",
+      to: "/components/horizontal-navigation",
+    },
+    {
+      label: "Command Palette",
+      icon: "i-heroicons-command-line",
+      to: "/components/command-palette",
+    },
+  ],
+  [
+    {
+      label: "Examples",
+      icon: "i-heroicons-light-bulb",
+    },
+    {
+      label: "Help",
+      icon: "i-heroicons-question-mark-circle",
+    },
+  ],
+];
+</script>
+
+
+<template>
+  <UHorizontalNavigation
+    :links="links"
+    class="border-b border-gray-200 dark:border-gray-800"
+  />
+</template>
+
+
+
+<style scoped></style>

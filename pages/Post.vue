f1c9791598926df0ebbf98092fb654fde7989e81
Andre Seiler <andre.01156@web.de>
2025-01-23 14:28:54 +0100

    Simples Post Form zum erstellen eines Posts

--- pages/Post.vue
+++ pages/Post.vue
@@ -1,3 +1,31 @@
+<script>
+</script>
+
+
 <template>
-    
+    <UCard>
+    <template #header>
+      <Placeholder class="h-8" />
+      <UTextarea :rows="1" color="primary" variant="outline" placeholder="Title..." />
+    </template>
+
+    <Placeholder class="h-32" />
+    <UTextarea autoresize color="gray" variant="outline" placeholder="Text..." />
+
+    <UContainer>
+    <Placeholder class="h-32" />
+    <img  :src="{imageLink}" hidden width="300" height="400" draggable="false">
+  </UContainer>
+    <template #footer>
+      <Placeholder class="h-8" />
+      <UButton
+    icon="i-heroicons-pencil-square"
+    size="sm"
+    color="primary"
+    variant="solid"
+    label="Post"
+    :trailing="false"
+  />
+    </template>
+  </UCard>
 </template>

7663a571f1104cf8ece209ceea9af73ed4390ee9
Aaron Turyabahika <abaturyabahika@gmail.com>
2025-01-24 11:04:46 +0100

    .prettierignore Datei hinzugefügt

--- app.vue
+++ app.vue
@@ -4,16 +4,17 @@
             <template #header>
                 <div class="flex justify-between">
                     <h1>Welcome to Nuxt UI Starter</h1>
-                    <ColorScheme
-                        ><USelect
+                    <ColorScheme>
+                        <USelect
                             v-model="$colorMode.preference"
                             :options="['system', 'light', 'dark']"
-                    /></ColorScheme>
+                        />
+                    </ColorScheme>
                 </div>
             </template>
-            <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank"
-                >Open Nuxt UI Documentation</UButton
-            >
+            <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">
+                Open Nuxt UI Documentation
+            </UButton>
         </UCard>
     </UContainer>
 </template>

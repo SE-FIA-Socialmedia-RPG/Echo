1039a7d0c89f02986fa72fae6a38147a74f02d5b
Andre Seiler <andre.01156@web.de>
2025-01-24 11:52:32 +0100

    #4 Erste Elemente im Header untergebracht

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -1,3 +1,25 @@
 <template>
+    <UCard>
+    <template #header>
+
+        <UAvatar
+    size="sm"
+    src="https://avatars.githubusercontent.com/u/739984?v=4"
+    alt="Profilbild"
+  />
+
+        <a>
+            Username
+        </a>
+        <UChip text="86" size="2xl" alt="Level">
+        <UMeter icon="line-md:chevron-double-up" :value="86" label="NächstesLevel Fortschritt" />
+        </UChip>
+    </template>
+
     
+
+    <template #footer>
+      
+    </template>
+  </UCard>
 </template>

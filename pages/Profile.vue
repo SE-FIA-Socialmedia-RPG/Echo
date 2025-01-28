e5dc98d5807627b37af19baecf3ea57010ba7ab1
Andre Seiler <andre.01156@web.de>
2025-01-28 14:49:41 +0100

    #4 Logik hinzugefügt die gewisse Funktionen nur zulässt und Elemente nur anzeigt wenn der Nutzer auch der Besitzer des Profils ist

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -7,6 +7,7 @@ export default {
     const isFollowing = ref(false);
     const isExpanded = ref(false);
     const showButton = ref(false);
+    const isProfileOwner = ref(true);
     const textContainer = ref<HTMLElement | null>(null);
 
     const clampedCheck = () => {
@@ -35,6 +36,7 @@ export default {
       isExpanded,
       showButton,
       textContainer,
+      isProfileOwner,
     };
   },
 };
@@ -44,20 +46,23 @@ export default {
   <div class="flex flex-col items-center p-6">
     <UCard class="w-full max-w-md">
       <template #header>
-        <div class="relative w-full h-16 rounded-lg overflow-hidden bg-gray-200 group">
-    <img
-      alt="banner"
-      src="https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQEl9Jg9SpIW1KgRr7PjJZW8SvYiJxNHFxaajauOClG1SucYo3bqQotWl21Xm_hE5Mjv1Io6QdANvNVzR_QToyfCv28GZlomvBA"
-      class="w-full h-full object-fill"
-    />
-    <UButton
-      icon="line-md:edit"
-      size="2xs"
-      color="white"
-      variant="solid"
-      class="absolute top-2 right-2 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
-    />
-  </div>
+        <div
+          class="relative w-full h-16 rounded-lg overflow-hidden bg-gray-200 group"
+        >
+          <img
+            alt="banner"
+            src="https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQEl9Jg9SpIW1KgRr7PjJZW8SvYiJxNHFxaajauOClG1SucYo3bqQotWl21Xm_hE5Mjv1Io6QdANvNVzR_QToyfCv28GZlomvBA"
+            class="w-full h-full object-fill"
+          />
+          <UButton
+            icon="line-md:edit"
+            size="2xs"
+            color="white"
+            variant="solid"
+            class="absolute top-2 right-2 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+            v-if="isProfileOwner"
+          />
+        </div>
         <div class="flex items-center space-x-4 mt-4">
           <UAvatar
             size="xl"
@@ -76,7 +81,7 @@ export default {
             </UChip>
           </div>
         </div>
-        <div class="flex space-x-4 flex-row mt-5">
+        <div class="flex space-x-3 flex-row mt-5">
           <UButton
             icon="line-md:account"
             size="sm"
@@ -88,7 +93,14 @@ export default {
 
           <div class="relative flex items-center">
             <UButton
-              v-if="!isFollowing"
+              v-if="isProfileOwner"
+              size="sm"
+              color="white"
+              variant="solid"
+              label="Bearbeiten"
+            />
+            <UButton
+              v-if="!isProfileOwner && !isFollowing"
               icon="line-md:account-add"
               size="sm"
               color="primary"
@@ -98,7 +110,7 @@ export default {
               class="transition duration-200 ease-in-out"
             />
             <UButton
-              v-else
+              v-if="!isProfileOwner && isFollowing"
               icon="material-symbols:person-check-outline"
               size="sm"
               color="white"
@@ -107,9 +119,8 @@ export default {
               @click="toggleFollow"
               class="transition duration-200 ease-in-out"
             />
-
             <UButton
-              v-if="isFollowing"
+              v-if="!isProfileOwner && isFollowing"
               icon="line-md:account-delete"
               size="sm"
               color="red"
@@ -141,7 +152,11 @@ export default {
                   class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mb-5 overflow-x-auto no-scrollbar pb-2 pr-2 pl-2"
                 >
                   <router-link to="/">
-                    <UBadge label="fünfL" color="gray" class="w-32 flex items-center justify-center">
+                    <UBadge
+                      label="fünfL"
+                      color="gray"
+                      class="w-32 flex items-center justify-center"
+                    >
                       <template #leading>
                         <UAvatar
                           src="https://avatars.githubusercontent.com/u/739984?v=4"
@@ -153,7 +168,11 @@ export default {
                     </UBadge>
                   </router-link>
                   <router-link to="/">
-                    <UBadge label="NurachtL" color="gray" class="w-32 flex items-center justify-center">
+                    <UBadge
+                      label="NurachtL"
+                      color="gray"
+                      class="w-32 flex items-center justify-center"
+                    >
                       <template #leading>
                         <UAvatar
                           src="https://avatars.githubusercontent.com/u/739984?v=4"
@@ -165,7 +184,11 @@ export default {
                     </UBadge>
                   </router-link>
                   <router-link to="/">
-                    <UBadge label="sechzehnzeichenL" color="gray" class="w-32 flex items-center justify-center">
+                    <UBadge
+                      label="sechzehnzeichenL"
+                      color="gray"
+                      class="w-32 flex items-center justify-center"
+                    >
                       <template #leading>
                         <UAvatar
                           src="https://avatars.githubusercontent.com/u/739984?v=4"
@@ -185,11 +208,7 @@ export default {
 
       <div class="flex flex-row mt-4">
         <div class="flex flex-col items-center w-24 border-r pr-4">
-          <img
-            src=""
-            class="w-16 h-12 mb-4"
-            alt="Abzeichen"
-          />
+          <img src="" class="w-16 h-12 mb-4" alt="Abzeichen" />
         </div>
         <div class="ml-5 w-72">
           <a
@@ -197,9 +216,22 @@ export default {
             :class="[!isExpanded ? 'line-clamp-6' : 'line-clamp-none']"
             class="text-md"
           >
-          Jemand musste Josef K. verleumdet haben, denn ohne dass er etwas Böses getan hätte, wurde er eines Morgens verhaftet. »Wie ein Hund!« sagte er, es war, als sollte die Scham ihn überleben. Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheueren Ungeziefer verwandelt. Und es war ihnen wie eine Bestätigung ihrer neuen Träume und guten Absichten, als am Ziele ihrer Fahrt die Tochter als erste sich erhob und ihren jungen Körper dehnte. »Es ist ein eigentümlicher Apparat«, sagte der Offizier zu dem Forschungsreisenden und überblickte mit einem gewissermaßen bewundernden Blick den
+            Jemand musste Josef K. verleumdet haben, denn ohne dass er etwas
+            Böses getan hätte, wurde er eines Morgens verhaftet. »Wie ein Hund!«
+            sagte er, es war, als sollte die Scham ihn überleben. Als Gregor
+            Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in
+            seinem Bett zu einem ungeheueren Ungeziefer verwandelt. Und es war
+            ihnen wie eine Bestätigung ihrer neuen Träume und guten Absichten,
+            als am Ziele ihrer Fahrt die Tochter als erste sich erhob und ihren
+            jungen Körper dehnte. »Es ist ein eigentümlicher Apparat«, sagte der
+            Offizier zu dem Forschungsreisenden und überblickte mit einem
+            gewissermaßen bewundernden Blick den
           </a>
-          <Ubutton class="text-gray-500 cursor-pointer" v-if="showButton" @click="isExpanded = !isExpanded">
+          <Ubutton
+            class="text-gray-500 cursor-pointer"
+            v-if="showButton"
+            @click="isExpanded = !isExpanded"
+          >
             {{ isExpanded ? "Weniger anzeigen" : "Mehr anzeigen" }}</Ubutton
           >
         </div>
@@ -208,21 +240,19 @@ export default {
       <template #footer>
         <div class="flex flex-row justify-between">
           <UButton
-    icon="material-symbols:add-2"
-    size="xs"
-    color="primary"
-    variant="solid"
-    label="New Post"
-    :trailing="false"
-  />
-        <UBadge color="gray" variant="solid">Posts: 157</UBadge>
-        
-</div>
+            icon="material-symbols:add-2"
+            size="xs"
+            color="primary"
+            variant="solid"
+            label="New Post"
+            :trailing="false"
+            v-if="isProfileOwner"
+          />
+          <UBadge color="gray" variant="solid">Posts: 157</UBadge>
+        </div>
       </template>
     </UCard>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>

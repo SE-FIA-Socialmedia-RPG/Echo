919fabc8ca448f5efdef4be859756ee982c9eeab
Leon Woenckhaus <leon.woenckhaus@siemens-energy.com>
2025-01-28 09:01:42 +0100

    #10 Api route für [id].get korriegiert

--- server/api/users/[id].get.ts
+++ server/api/users/[id].get.ts
@@ -6,19 +6,26 @@ const prisma = new PrismaClient();
 
 // Exportiere den Event-Handler als Standard-Export
 export default defineEventHandler(async (event) => {
-    // Extrahiere die benötigten Daten aus dem Body der Anfrage
-    const { id } = getQuery(event);
-        try {
-            return await prisma.User.findUnique({
-                where: {
-                id: id // Zielnutzer anhand der ID identifizieren
-                },
-            });
-        } catch (error) {
+    // Extrahiere die benötigten Daten aus den Parametern der Anfrage
+    const { id } = event.context.params;
+
+    try {
+        // Finde einen einzigartigen Benutzer anhand der ID
+        const ret = await prisma.User.findUnique({
+            where: {
+                id: parseInt(id) // Konvertiere die ID zu einer Ganzzahl und suche den Benutzer
+            },
+        });
+        // Überprüfe, ob der Benutzer gefunden wurde und gib das Ergebnis zurück
+        return ret ? ret : {
+            statusCode: 404,
+            message: "User not found", // Benutzer nicht gefunden
+        };
+    } catch (error) {
         // Fehlerhandling für Datenbankprobleme während der Abfrage
-             return {
-                statusCode: 400,
-                message: `Database request failed: ${error.message}`, // Fehlerdetails an den Client weitergeben
-            };
-        }
+        return {
+            statusCode: 400,
+            message: `Database request failed: ${error.message}`, // Fehlerdetails an den Client weitergeben
+        };
+    }
 });

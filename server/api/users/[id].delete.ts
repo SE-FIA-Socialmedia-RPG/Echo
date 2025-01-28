4d8f487f7ec777734c7fb78db5707f25f4e936cd
Leon Woenckhaus <leon.woenckhaus@siemens-energy.com>
2025-01-28 09:11:45 +0100

    #10 Api route für delete by id eingefügt, AUTHENTIFIZIERUNG FEHLT

---
+++ server/api/users/[id].delete.ts
@@ -0,0 +1,31 @@
+// Importiere PrismaClient aus dem Prisma-ORM-Paket
+import { PrismaClient } from '@prisma/client'
+
+// Initialisiere eine Instanz des PrismaClient
+const prisma = new PrismaClient();
+
+// Exportiere den Event-Handler als Standard-Export
+export default defineEventHandler(async (event) => {
+    // Extrahiere die benötigten Daten aus den Parametern der Anfrage
+    const { id } = event.context.params;
+
+    try {
+        // Finde einen einzigartigen Benutzer anhand der ID
+        const ret = await prisma.User.delete({
+            where: {
+                id: parseInt(id) // Konvertiere die ID zu einer Ganzzahl und suche den Benutzer
+            },
+        });
+        // Überprüfe, ob der Benutzer gefunden wurde und gib das Ergebnis zurück
+        return ret ? { message: `Eintrag mit ID ${id} wurde gelöscht.` } : {
+            statusCode: 404,
+            message: "User not found", // Benutzer nicht gefunden
+        };
+    } catch (error) {
+        // Fehlerhandling für Datenbankprobleme während der Abfrage
+        return {
+            statusCode: 400,
+            message: `Database request failed: ${error.message}`, // Fehlerdetails an den Client weitergeben
+        };
+    }
+});

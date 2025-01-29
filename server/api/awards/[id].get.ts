0b29c689fdea293e71eacd9aff8d3b80812fc2d2
Leon Woenckhaus <leon.woenckhaus@siemens-energy.com>
2025-01-29 11:43:18 +0100

    #19 API Routen für Awards, needs review

---
+++ server/api/awards/[id].get.ts
@@ -0,0 +1,40 @@
+// Importiere PrismaClient aus dem Prisma-ORM-Paket
+import { PrismaClient } from '@prisma/client'
+
+// Initialisiere eine Instanz des PrismaClient
+const prisma = new PrismaClient();
+
+// Exportiere den Event-Handler als Standard-Export
+export default defineEventHandler(async (event) => {
+    // Extrahiere die benötigten Daten aus den Parametern der Anfrage
+    const { id } = event.context.params;
+
+    try {
+        // Finde einen einzigartigen Benutzer anhand der ID
+        const ret = await prisma.award.findUnique({
+            where: {
+                id: parseInt(id) // Konvertiere die ID zu einer Ganzzahl und suche den Benutzer
+            },
+            select: {
+                id: true,
+                name: true,
+                awardImageId: true,
+                CommunityId: true,
+                createdAt: true,
+                updatedAt: true,
+                users: true 
+            },
+        });
+        // Überprüfe, ob der Benutzer gefunden wurde und gib das Ergebnis zurück
+        return ret ? ret : {
+            statusCode: 404,
+            message: "User not found", // Benutzer nicht gefunden
+        };
+    } catch (error) {
+        // Fehlerhandling für Datenbankprobleme während der Abfrage
+        return {
+            statusCode: 400,
+            message: "Database request failed", // Fehlerdetails an den Client weitergeben
+        };
+    }
+});

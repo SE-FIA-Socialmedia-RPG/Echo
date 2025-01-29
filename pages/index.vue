d44954f737bc735b8fd1c1f34f3a7d235bf2ffea
Ogulcan Kuecuek <ogulcankucuk05@gmail.com>
2025-01-29 11:09:44 +0100

    API Test-Branch erstellen

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
+  <UButton
+    icon="i-heroicons-pencil-square"
+    size="sm"
+    color="primary"
+    variant="solid"
+    label="Button"
+    :trailing="false"
+  />
 
 </template>

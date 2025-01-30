46e75ce4dd21ea035e7db5e7a7f8708a0b8a0847
Ogulcan Kuecuek <ogulcankucuk05@gmail.com>
2025-01-30 08:48:40 +0100

    API euseFetch + Funktionalität hergestellt

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,51 @@
 <template>
     <UContainer>
-    <UCard class="mt-10">
-        <template #header>
-        <div class="flex justify-between">
-        <h1 style="font-size: 25px;"><b>Test, Test...</b></h1>
-        </div>
-        <br>
-        <form action="registrierung.php" method="post">
-            <!--Input-Box-->
-            <UTooltip text="Undefinierter Input. Such dir was aus (im Quellcode)!" :popper="{ placement: 'right' }">
-            <label for="input">Input:</label>
-            </UTooltip>
-            <UInput size="sm" input type="text" id="input" name="input" required color="white" variant="outline" placeholder="Input" />
-            <p></p>
-            <br>
-            <UButton loading-icon="svg-spinners:bars-rotate-fade" icon="ep:d-arrow-right" color="primary" variant="solid" type="submit">Absenden</UButton>
-        </form> 
-      </template>
-      <template #footer>
-        <div class="flex justify-between">
-        <h1 style="font-size: 25px;"><b>Output:</b></h1>
-        </div>
-        <br>
-        <UContainer>
-        <!--Output-Box-->
-        <div class="box-rounded">
-            XYZ
-            <br>
-            123
-            <br>
-            ABC
-        </div>
-      </UContainer>
-      </template>
-    </UCard>
-  </UContainer>
+        <UCard class="mt-10">
+            <template #header>
+                <div class="flex justify-between">
+                    <h1 style="font-size: 25px;"><b>Test, Test...</b></h1>
+                </div>
+                <br>
+                <!--Input-Box-->
+                <UTooltip text="Undefinierter Input. Such dir was aus (im Quellcode)!" :popper="{ placement: 'right' }">
+                    <label for="input">Input:</label>
+                </UTooltip>
+                <UInput v-model="id" size="sm" type="number" required color="white" variant="outline"
+                    placeholder="Input" />
+                <p></p>
+                <br>
+                <UButton loading-icon="svg-spinners:bars-rotate-fade" icon="material-symbols-light:refresh"
+                    color="primary" variant="solid" :loading="status === 'pending'" @click="refresh()">Refresh</UButton>
+            </template>
+            <template #footer>
+                <div class="flex justify-between">
+                    <h1 style="font-size: 25px;"><b>Output:</b></h1>
+                </div>
+                <br>
+                <UContainer>
+                    <!--Output-Box-->
+                    <div class="box-rounded">
+                        {{ data }}
+                    </div>
+                </UContainer>
+            </template>
+        </UCard>
+    </UContainer>
 </template>
 
-<script setup>
+<script setup lang="ts">
+const id = ref(1)
+
+const { data, status, refresh } = useFetch(() => '/api/users/' + id.value)
+
+/*
+const response = await fetch(baseUrl + 'users?id=' + id)
+
+return await response.json()
+*/
 </script>
 
-<!--
-<script passwortWiederholung>
-if(passwort === passwortWiederholt){
-    console.log('Passwort korrekt')
-  }
-</script>
--->
-
-<style scoped> 
+<style scoped>
 .centered {
     text-align: center;
 }
@@ -58,12 +55,10 @@ if(passwort === passwortWiederholt){
 }
 
 .box-rounded {
-  width: auto;
-  height: auto;
-  padding: 10px;
-  border: 2px solid rgb(229, 229, 229);
-  border-radius: 15px;
+    width: auto;
+    height: auto;
+    padding: 10px;
+    border: 2px solid rgb(229, 229, 229);
+    border-radius: 15px;
 }
-
 </style>
-  

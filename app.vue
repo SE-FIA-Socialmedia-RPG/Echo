8620d277d09a5863bc120caca2420bbdb7dc8b49
Aaron Turyabahika <abaturyabahika@gmail.com>
2025-01-30 11:47:19 +0100

    #21 Bild-Upload Komponente

---
+++ app.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ImageUpload from "@/components/ImageUpload.vue";
+</script>
+
+<template>
+  <ImageUpload />
+</template>
---
+++ components/ImageUpload.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex justify-center mt-40 border-10 border-white border-solid p-10"
+  >
+    <input
+      type="file"
+      icon="i-heroicons-file"
+      size="sm"
+      class="hidden hover:bg-blue-700"
+      ref="fileElem"
+      accept="image/png, image/jpeg, image/jpeg, image/webp"
+      @change="onFileChanged($event)"
+    />
+    <UButton type="button" @click="onInputClicked()"> Datei auswählen</UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const file = ref(null);
+const fileInput = useTemplateRef("fileElem");
+const onFileChanged = (event: Event) => {
+  const files = (<HTMLInputElement>event.target!).files;
+};
+const onInputClicked = () => {
+  fileInput.value?.click();
+};
+</script>

a565ffaf0e9c223a655c6750e6a428cd2f15a05f
Ogulcan Kuecuek <ogulcankucuk05@gmail.com>
2025-01-30 12:25:20 +0100

    Umwandlung in Komponent & Schleifen für Modularität DRAFT

---
+++ components/VotingOption.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type Props = {
+    votingOptionID: int
+    prozentBar: byte
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+    <UCheckbox label="Option Name" help="Beschreibung" />
+    <UProgress :value="prozentBar" />
+    <br>
+</template>

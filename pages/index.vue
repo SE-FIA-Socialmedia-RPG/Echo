53d848c0ff7b96b3171ead95a12977643308f4b2
Ogulcan Kuecuek <ogulcankucuk05@gmail.com>
2025-01-31 10:28:46 +0100

    Feed-Loading Schleife erstellt (Generierung von Posts, der Post wird noch nicht geladen)

--- pages/index.vue
+++ pages/index.vue
@@ -6,35 +6,52 @@
         <br>
     </UContainer>
     <UContainer>
-        <PostFeedAnsicht />
-        <br>
-        <br>
-        <PostFeedAnsicht />
-        <br>
-        <br>
-        <PostFeedAnsicht />
+        <div v-for="index in pages" :key="index">
+            <PostFeedAnsicht />
+            <br>
+            <br>
+        </div>
     </UContainer>
 </template>
 
 <script lang="ts">
+import { defineComponent, ref } from 'vue';
 import NeuerPost from '~/components/NeuerPost.vue';
 import PostFeedAnsicht from '~/components/PostFeedAnsicht.vue';
 
-    
-    let likeGedrueckt = false
-    
-    function likeDruecken() {
-        likeGedrueckt = true
-        //Methode zum Speichern des Likes in der Datenbank
+export default defineComponent({
+  components: {
+    NeuerPost,
+    PostFeedAnsicht
+  },
+  
+  setup() {
+    const pages = ref(25)
+    const likeGedrueckt = ref(false)
+
+    const likeDruecken = () => {
+      likeGedrueckt.value = true
+      // Methode zum Speichern des Likes in der Datenbank
     }
 
-    function oeffneKommentare() {
-        //Methode zum öffnen (und Posten) der Kommentare
+    const oeffneKommentare = () => {
+      // Methode zum Öffnen (und Posten) der Kommentare
+    }   
+
+    const teilePost = () => {
+      // Methode zum Kopieren des Post-Links (und evt. auch weitere Optionen zum Teilen)
     }
 
-    function teilePost() {
-        //Methode zum kopieren des Post-Link (und evt. auch weitere Optionen zum Teilen)
+    return {
+      pages,
+      likeGedrueckt,
+      likeDruecken,
+      oeffneKommentare,
+      teilePost
     }
+  }
+})
+
 </script>
 
 <style>

01893eadf2a3525e21870a0a45c479f5a3342be9
Friedrich Waack <friedrich.waack@programado.de>
2025-02-12 12:18:39 +0100

    #18 - remove hardcoded image path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,6 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@prisma/nuxt"],
   compatibilityDate: "2025-01-23",
   runtimeConfig: {
-    imagesPath: process.env.NUXT_IMAGE_PATH || '/Users/friedrich/code/siemens/2025/Echo/images'
+    imagesPath: process.env.NUXT_IMAGE_PATH
   },
 })

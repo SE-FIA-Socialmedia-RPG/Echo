be3afb804d5b44a209394b123768a819752827ee
Aaron Turyabahika <abaturyabahika@gmail.com>
2025-02-13 11:02:26 +0100

    #17 COmmunity View - Formatting Issues

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
-
+<script setup lang="ts">
 import {ref} from 'vue'
 import NavigationBar from '@/components/NavigationBar.vue'
+import CommunityView from '@/components/CommunityView.vue'
 
 const appName = ref<string>('Social Media RPG')
 </script>
@@ -8,8 +9,7 @@ const appName = ref<string>('Social Media RPG')
 <template>
     <div id="app">
         <NavigationBar />
-
+        <CommunityView />
         <NuxtPage />
     </div>
-
 </template>

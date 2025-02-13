e1cb1d386aac51fdfef763465adee2d3e565d78c
Andre Seiler <andre.01156@web.de>
2025-02-13 10:37:09 +0100

    #4 Profilbild wird nun aus der API gezogen und angezeigt und Css wurde mit einem Prefixer bearbeitet um in allen Browsern problemlos zu funktionieren

--- components/ProfileAvatar.vue
+++ components/ProfileAvatar.vue
@@ -1,11 +1,6 @@
 <template>
     <div class="relative inline-block">
-        <UAvatar
-            size="xl"
-            src="https://th.bing.com/th/id/OIP.JfZ_C0McVR-AvQDLyCW0VwHaEK?rs=1&pid=ImgDetMain"
-            alt="Profilbild"
-            class="rounded-full"
-        />
+        <UAvatar size="xl" :src="src" alt="Profilbild" class="rounded-full" />
         <div :class="`absolute inset-0 ${frameClass} rounded-full`"></div>
     </div>
 </template>
@@ -15,6 +10,7 @@ import {defineProps} from 'vue'
 
 const props = defineProps<{
     frameClass?: string
+    src: string
 }>()
 
 const frameClass = props.frameClass || 'border-4 border-blue-500'
--- pages/Profile.vue
+++ pages/Profile.vue
@@ -2,7 +2,7 @@
 import {UButton} from '#components'
 import {resolveDirective} from 'vue'
 
-const userId = ref(7)
+const userId = ref(5)
 const isExpanded = ref(false)
 const showButton = ref(true)
 const textContainer = ref<HTMLElement | null>(null)
@@ -289,7 +289,10 @@ const unfollow = () => {
                 <div class="flex flex-row justify-between">
                     <div class="flex items-center space-x-4 mt-4">
                         <div class="flex space-x-4">
-                            <ProfileAvatar frameClass="border-4 border-primary-500" />
+                            <ProfileAvatar
+                                frameClass="border-4 border-primary-500"
+                                :src="user.profileImage"
+                            />
                         </div>
 
                         <div class="flex flex-col">
@@ -546,7 +549,7 @@ const unfollow = () => {
                                                     >
                                                     <UAvatar
                                                         size="3xl"
-                                                        src="https://avatars.githubusercontent.com/u/739984?v=4"
+                                                        :src="user.profileImage"
                                                         alt="Profilbild"
                                                     />
                                                 </div>
@@ -963,7 +966,18 @@ const unfollow = () => {
 
 .animated-sparkle {
     color: #ffcc00;
+    -webkit-animation: sparkle-animation 1.5s infinite;
     animation: sparkle-animation 1.5s infinite;
+}
+
+@-webkit-keyframes sparkle-animation {
+    0%,
+    100% {
+        text-shadow: 0 0 5px #ffcc00, 0 0 10px #ffcc00;
+    }
+    50% {
+        text-shadow: 0 0 20px #ffcc00, 0 0 30px #ffcc00;
+    }
 }
 
 @keyframes sparkle-animation {
@@ -980,7 +994,17 @@ const unfollow = () => {
 .animated-glow {
     color: #00ffcc;
     text-shadow: 0 0 5px #00ffcc, 0 0 10px #00ffcc, 0 0 15px #00ffcc;
+    -webkit-animation: glow-animation 1.5s infinite alternate;
     animation: glow-animation 1.5s infinite alternate;
+}
+
+@-webkit-keyframes glow-animation {
+    0% {
+        text-shadow: 0 0 5px #00ffcc, 0 0 10px #00ffcc, 0 0 15px #00ffcc;
+    }
+    100% {
+        text-shadow: 0 0 20px #00ffcc, 0 0 30px #00ffcc, 0 0 40px #00ffcc;
+    }
 }
 
 @keyframes glow-animation {
@@ -995,17 +1019,36 @@ const unfollow = () => {
 /* Wellen Name */
 .animated-wave {
     color: #ff6347;
+    -webkit-animation: wave-animation 1.5s infinite;
     animation: wave-animation 1.5s infinite;
+}
+
+@-webkit-keyframes wave-animation {
+    0% {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
+    50% {
+        -webkit-transform: translateY(-5px);
+        transform: translateY(-5px);
+    }
+    100% {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
 }
 
 @keyframes wave-animation {
     0% {
+        -webkit-transform: translateY(0);
         transform: translateY(0);
     }
     50% {
+        -webkit-transform: translateY(-5px);
         transform: translateY(-5px);
     }
     100% {
+        -webkit-transform: translateY(0);
         transform: translateY(0);
     }
 }
@@ -1013,7 +1056,18 @@ const unfollow = () => {
 /* Blitzender Name */
 .animated-flash {
     color: #ffcc00;
+    -webkit-animation: flash-animation 1s infinite;
     animation: flash-animation 1s infinite;
+}
+
+@-webkit-keyframes flash-animation {
+    0%,
+    100% {
+        opacity: 1;
+    }
+    50% {
+        opacity: 0.5;
+    }
 }
 
 @keyframes flash-animation {
@@ -1034,7 +1088,7 @@ const unfollow = () => {
 
 /* Schattierter Name */
 .text-shadow {
-    color: #4a5568; /* Tailwind's gray-700 */
+    color: #4a5568;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 }
 
@@ -1046,12 +1100,31 @@ const unfollow = () => {
 
 /* Farbverlauf Name */
 .gradient-text {
+    background: -webkit-gradient(
+        linear,
+        left top,
+        right top,
+        from(#ff0080),
+        color-stop(#ff8c00),
+        to(#ffd700)
+    );
+    background: -o-linear-gradient(left, #ff0080, #ff8c00, #ffd700);
     background: linear-gradient(90deg, #ff0080, #ff8c00, #ffd700);
 
     -webkit-text-fill-color: transparent;
 }
 
 .gradient-text {
+    background: -o-linear-gradient(
+        45deg,
+        #ff0000,
+        #ff9900,
+        #ffff00,
+        #00ff00,
+        #00ffff,
+        #0000ff,
+        #ff00ff
+    );
     background: linear-gradient(
         45deg,
         #ff0000,
@@ -1066,7 +1139,20 @@ const unfollow = () => {
     color: transparent;
     -webkit-background-clip: text;
     background-clip: text;
+    -webkit-animation: gradient-animation 3s ease infinite;
     animation: gradient-animation 3s ease infinite;
+}
+
+@-webkit-keyframes gradient-animation {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
 }
 
 @keyframes gradient-animation {
@@ -1084,7 +1170,17 @@ const unfollow = () => {
 .animated-electric {
     color: #00ffff;
     text-shadow: 0 0 10px #00ffff, 0 0 20px #00ffff;
+    -webkit-animation: electric-animation 1.5s infinite alternate;
     animation: electric-animation 1.5s infinite alternate;
+}
+
+@-webkit-keyframes electric-animation {
+    0% {
+        text-shadow: 0 0 10px #00ffff, 0 0 20px #00ffff;
+    }
+    100% {
+        text-shadow: 0 0 20px #00ffff, 0 0 40px #00ffff;
+    }
 }
 
 @keyframes electric-animation {
@@ -1097,11 +1193,25 @@ const unfollow = () => {
 }
 
 .animated-mystic-rainbow {
+    background: -o-linear-gradient(45deg, #ff00ff, #00ffff, #ff69b4);
     background: linear-gradient(45deg, #ff00ff, #00ffff, #ff69b4);
     -webkit-text-fill-color: transparent;
+    -webkit-animation: mystic-rainbow-animation 3s ease infinite;
     animation: mystic-rainbow-animation 3s ease infinite;
     -webkit-background-clip: text;
     background-clip: text;
+}
+
+@-webkit-keyframes mystic-rainbow-animation {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
 }
 
 @keyframes mystic-rainbow-animation {

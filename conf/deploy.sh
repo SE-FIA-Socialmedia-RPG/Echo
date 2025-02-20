5f0308ad41fef14243726eaeaec69eb8fe8f7758
Nick Hildebrandt <N.Hildebrandt@ts13.de>
2025-02-20 15:53:26 +0100

    Deployment Skript und config Dateien

---
+++ conf/deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sed -e
+
+ECHO_PATH="/opt/echo"
+ECHO_URL="git@github.com:SE-FIA-Socialmedia-RPG/Echo.git"
+
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Error: This script must be run as root!" >&2
+    exit 1
+fi
+
+if ! (grep -qs 'ID=debian' /etc/os-release && systemctl > /dev/null 2>&1); then
+  echo "Error: Not on a Debian system with systemd" >&2
+  exit 1
+fi
+
+for cmd in git node npm; do
+  if ! command -v $cmd &> /dev/null; then
+    echo "Error: $cmd is not installed." >&2
+    exit 1
+  fi
+done
+
+if systemctl is-active --quiet "echo.service"; then
+    systemctl stop "echo.service"
+fi
+
+if [[ -d "${ECHO_PATH}" ]]; then
+    rm -rf "${ECHO_PATH}"
+fi
+
+mkdir -p "${ECHO_PATH}"
+git clone "${ECHO_URL}" "${ECHO_PATH}"
+
+chown -R www-data:www-data "${ECHO_PATH}"
+
+(cd "${ECHO_PATH}"
+npm install
+npx prisma migrate dev --name dev
+npm run build
+)
+
+cp "${ECHO_PATH}/conf/env" "${ECHO_PATH}/.output/server/.env"
+cp "${ECHO_PATH}/conf/echo.service" "/etc/systemd/system/echo.service"
+cp "${ECHO_PATH}/conf/nginx.conf" "/etc/nginx/nginx.conf"
+
+systemctl daemon-reload
+systemctl enable "echo.service"
+systemctl enable "nginx.service"
+systemctl restart "echo.service"
+systemctl restart "nginx.service"

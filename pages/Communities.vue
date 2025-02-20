aac0a180d772e52032e788efaf83222c8707dc48
Aaron Turyabahika <abaturyabahika@gmail.com>
2025-02-20 09:42:10 +0100

    Worked on Search Function

--- pages/Communities.vue
+++ pages/Communities.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import CommunitySearchResults from '~/components/SearchResults/CommunitySearchResults.vue'
+const searchInput = ref('')
+const users = ref([])
+const posts = ref([])
+const selectedTab = ref(1)
 const isOpen = ref(false)
 const member = ref(false)
 const communities = ref<Community[]>([])
 const query = ref('')
+
+interface Community {
+    id: number
+    communityName: string
+    _count: {
+        users: number
+    }
+    bannerImage: {
+        path: string
+    }
+    description: string
+}
 
 async function getCommunities() {
     try {
@@ -18,17 +35,46 @@ onMounted(() => {
     getCommunities()
 })
 
-interface Community {
-    id: number
-    communityName: string
-    _count: {
-        users: number
+const items = [
+    {
+        label: 'Communities',
+        icon: 'i-heroicons-arrow-down-tray',
+        key: 1,
+    },
+]
+
+const handleSearchClick = async () => {
+    try {
+        const [communitiesResponse] = await Promise.all([
+            $fetch(`/api/communities/search`, {
+                method: 'POST',
+                body: {
+                    query: searchInput.value,
+                },
+            }),
+        ])
+
+        console.log('Communities:', communitiesResponse)
+
+        communities.value = communitiesResponse
+
+        return {communitiesResponse}
+    } catch (error) {
+        console.error('Error fetching search:', error)
+        throw error
     }
-    bannerImage: {
-        path: string
-    }
-    description: string
 }
+
+const selectedContent = computed(() => {
+    const selectedItem = items.find((item) => item.key === selectedTab.value)
+    console.log('Selected Tab:', selectedTab.value) // Überprüfe die Änderungen der selectedTab-Variable
+    console.log('Selected Item:', selectedItem) // Überprüfe das ausgewählte Item
+    return selectedItem ? selectedItem.key : ''
+})
+// Überwache die Änderungen der selectedTab-Variable
+watch(selectedTab, (newVal, oldVal) => {
+    console.log(`Tab changed from ${oldVal} to ${newVal}`)
+})
 </script>
 
 <template>
@@ -38,6 +84,18 @@ interface Community {
                 <div class="h-24">
                     <p class="text-2xl">Communities</p>
                     <div class="flex justify-end items-center">
+                        <div class="flex justify-end items-center">
+                            <input
+                                class="border-green-600"
+                                v-model="searchInput"
+                                placeholder="Search for keyword"
+                            />
+                            <UButton
+                                icon="i-heroicons-magnifying-glass"
+                                @click="handleSearchClick"
+                                class="mr-5 ml-5"
+                            />
+                        </div>
                         <UToggle size="lg" v-model="member" on-icon="i-heroicons-check-badge">
                         </UToggle>
                     </div>
@@ -72,6 +130,19 @@ interface Community {
                                 </p>
                             </div>
                         </template>
+                        <UTabs v-model="selectedTab" :items="items">
+                            <template #item="{item}" class="w-full">
+                                <div v-if="selectedContent === 0">
+                                    <UserSearchResults :users="users" />
+                                </div>
+                                <div v-if="selectedContent === 1">
+                                    <CommunitySearchResults :communities="communities" />
+                                </div>
+                                <div v-if="selectedContent === 2">
+                                    <PostSearchResults :posts="posts" />
+                                </div>
+                            </template>
+                        </UTabs>
                     </UCard>
                 </div>
             </template>

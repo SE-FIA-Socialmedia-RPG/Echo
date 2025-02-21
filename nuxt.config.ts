744fcb42fcdc1c8be40e67907da6a358e4c5359b
Friedrich Waack <friedrich.waack@programado.de>
2025-02-21 08:20:32 +0100

    Fix formatting in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
     vite: {
         resolve: {
             alias: {
-		'.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js'
-            }
-        }
-    }
+                '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
+            },
+        },
+    },
 })

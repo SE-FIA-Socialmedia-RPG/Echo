08b4a4869ca22159c245dab7601b0bd207350d60
Friedrich Waack <friedrich.waack@programado.de>
2025-02-21 11:41:47 +0100

    Add temporary console log for debugging purposes

--- pages/search.vue
+++ pages/search.vue
@@ -9,6 +9,8 @@ const communities = ref<Community[]>([])
 const users = ref<User[]>([])
 const posts = ref<Post[]>([])
 
+console.log('test')
+
 const items = computed(() => [
     {
         label: 'Benutzer' + (hasSearched.value ? ` (${users.value.length})` : ''),
